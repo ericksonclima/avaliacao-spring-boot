6715994d55d909de448d2497f032e811f563a5ec
Erick Lima <Erick Lima@DESKTOP-7A00JBC>
2020-07-02 11:23:03 -0300

    Entidade Estudante

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -1,7 +1,11 @@
 package br.com.tokiomarine.seguradora.avaliacao.entidade;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 /**
  * Entidade que representa estudante
@@ -11,5 +15,56 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "estudantes")
 public class Estudante {
+	
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(name="nome")
+	@NotNull
+	private String nome;
+	
+	@Column(name="email")
+	@NotNull
+	private String email;
+	
+	@Column(name="telefone")
+	private String telefone;
+
+	public Estudante() {
+		
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 	
 }
